63015c710bf37f1fd90837d1258bacaddd64f19a
Tom Brown <Tom.Brown@lta.org.uk>
2016-02-26 15:16:50 +0000

    added logging with DocumentDB

--- BasicWebsite/BasicWebsite/Controllers/HomeController.cs
+++ BasicWebsite/BasicWebsite/Controllers/HomeController.cs
@@ -1,20 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using BusinessLogic.Models;
 
 namespace BasicWebsite.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        {            
             return View();
         }
 
         public ActionResult About()
         {
+            BusinessLogic.DocumentDBLogger logger = new BusinessLogic.DocumentDBLogger();
+            WebsiteLog log = new WebsiteLog();
+            log.Exception = new Exception("exception message!", new Exception("inner exception message!"));
+            log.Page = HttpContext.Request.Url.AbsoluteUri;
+            logger.Log(log);
+
             ViewBag.Message = "Your application description page.";
 
             return View();
---
+++ BasicWebsite/BusinessLogic/Models/WebsiteLog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BusinessLogic.Models
+{
+    public sealed class WebsiteLog
+    {
+        private string _id;
+        private DateTime _date;
+
+        public WebsiteLog()
+        {
+            _id = Guid.NewGuid().ToString();
+            _date = DateTime.Now;
+        }
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return _date;
+            }
+        }
+
+        public string Page { get; set; }
+
+        public Exception Exception { get; set; }
+    }
+}
